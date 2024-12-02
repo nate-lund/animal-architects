05aa6a2fdbcf5728bbf8c9432428679996841410
nate-lund <lund1736@umn.edu>
2024-12-02 11:35:18 -0600

    changed formating on 01 to make formatting prettier

--- bookdown_architecture.R
+++ bookdown_architecture.R
@@ -4,11 +4,15 @@
 # https://bookdown.org/yihui/bookdown/
 # https://happygitwithr.com/
   
-install.packages("bookdown", dependencies = TRUE)
+#packages for book making
+install.packages(c("knitr", "usethis", "bookdown"))
+install.packages(c("rmarkdown"))
 
 library(usethis)
 library(bookdown)
 library(knitr)
+library(rmarkdown)
+
 
 # the function used to compile the book, run before commiting
 render_book("index.Rmd", output_format = "bookdown::gitbook", output_dir = "docs",  new_session = TRUE)
--- docs/01_about.md
+++ docs/01_about.md
@@ -1,9 +1,4 @@
----
-title: "01_about"
-author: "Nate Lund"
-date: "2024-12-02"
-output: html_document
----
+
 
 # About
 
