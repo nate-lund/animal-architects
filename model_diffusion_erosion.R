83469923cfd62bbbe1ab89bf5a8bd2597b434213
nate-lund <lund1736@umn.edu>
2025-01-02 07:47:19 -0600

    X

--- model_diffusion_erosion.R
+++ model_diffusion_erosion.R
@@ -4,7 +4,7 @@ install.packages("ggplot2", "dplyr", "tidyr", "rlang")
 #'[run below]
 
 # set wd
-setwd("C:/Users/natha/OneDrive/Onedrive Documents/Graduate School/01_Projects/P4 Bioturbation and Soil Development Lit Review/My models")
+setwd("C:/Users/natha/OneDrive/Onedrive Documents/Graduate School/01_Projects/P0_Git_Projects/animal-architects")
 
 # data processing
 library(ggplot2)
@@ -43,26 +43,15 @@ df2[["z"]][c(2, 3, 4, 5, 6, 7, 8)] = data.frame(0, h, h*2, h*3, h*4, h*5, h*6)
 df2[["bd"]][c(2, 3, 4, 5, 6, 7)] = data.frame(1, 1, 1, 1, 1, 1)
 
 #' [set] starting included and excluded components of each layer, A:F
-df2[["inc"]][c(2, 3, 4, 5, 6, 7)] = data.frame(40, 20, 20, 20, 20, 20)
+df2[["inc"]][c(2, 3, 4, 5, 6, 7)] = data.frame(40, 0, 0, 0, 0, 0)
 df2[["exc"]][c(2, 3, 4, 5, 6, 7)] = data.frame(5, 5, 5, 5, 5, 5)
-
-print("df done :)")
 
 #'###################### [defining functions] #######################
 
 # define diffusion (bioturbation) function (if statements needed for top and bottom layers) (m/yr)
 Dz <- function(z) {
-  if(z == 0) {
-    0 # forces D(z) to zero at surface
-  } else {
-    if(z == h*6) {
-      0 # forces D(z) to zero at bottom
-    } else {
-      #' [set] diffusion function
-      0.005334 * exp(-z/0.28) # exponential (Johnson et al., 2014) for eq. (Yeates et a.l, 1995) for intercept
-      # 6.9 * 10^-5 + -1.06 * 10^-4 * z # linear
-    }
-  }
+  0.005334 * exp(-z/0.28) # exponential (Johnson et al., 2014) for eq. (Yeates et a.l, 1995) for intercept
+  # 6.9 * 10^-5 + -1.06 * 10^-4 * z # linear
 }
 
 # diffusion w/ depth function plotting
@@ -73,16 +62,31 @@ ggplot(data.frame(z = c(0, 3)), aes(x = z)) +
   scale_x_reverse(name = "depth (m)") +
   coord_flip()
 
+
 # define function that calculates local inc-diffusion (g/m/yr) into/out of ith layer
-clay_diff_fun <- function(Ch, Ci, Cj, zi, bdi) {
-  (Dz(z = zi) * (Ch - Ci) +
-     (Dz(z = zi + h) * (Cj - Ci)) )
+inc_diff_fun <- function(yh, yi, yj, zi, bdi) {
+  if(zi == 0) {
+    Dz(z = (zi + h)) * (yj - yi) # surface
+  } else {
+    if(zi == h * 5) {
+      Dz(z = zi) * (yh - yi) # bottom
+    } else {
+      Dz(z = zi) * yh + Dz(z = (zi + h)) * yj - yi * (Dz(z = zi) + Dz(z = zi +h)) # middle
+    }
+  }
 }
 
 # define function that calculates local exc-diffusion (g/m/yr) into/out of ith layer
-stones_diff_fun <- function(Sh, Si, Sj, zi, bdi) {
-  (Dz(z = zi) * Sh -
-     (Dz(z = zi + h) * Si) )
+exc_diff_fun <- function(yh, yi, yj, zi, bdi) {
+  if(zi == 0) {
+    Dz(z = (zi + h)) * (0 - yi) # surface
+  } else {
+    if(zi == h * 5) {
+      Dz(z = zi) * yh # bottom
+    } else {
+      Dz(z = zi) * yh - yi * Dz(z = zi +h) # middle
+    }
+  }
 }
 
 #'###################### [model calculations] #######################
@@ -94,7 +98,7 @@ for(crow in 2:(time_steps)) {
     df2[["inc"]][crow, ccol] = 
       df2[["inc"]][crow - 1, ccol] +
       step_years * 
-      clay_diff_fun(df2[["inc"]][crow - 1, ccol - 1],
+      inc_diff_fun(df2[["inc"]][crow - 1, ccol - 1],
                    df2[["inc"]][crow - 1, ccol],
                    df2[["inc"]][crow - 1, ccol + 1],
                    df2[["z"]][crow, ccol],
@@ -103,15 +107,13 @@ for(crow in 2:(time_steps)) {
     df2[["exc"]][crow, ccol] = 
       df2[["exc"]][crow - 1, ccol] +
       step_years * 
-      stones_diff_fun(df2[["exc"]][crow - 1, ccol - 1],
+      exc_diff_fun(df2[["exc"]][crow - 1, ccol - 1],
                     df2[["exc"]][crow - 1, ccol],
                     df2[["exc"]][crow - 1, ccol + 1],
                     df2[["z"]][crow, ccol],
                     df2[["bd"]][crow, ccol])
   } 
 }
-
-print("model done")
 
 #'###################### [creating a plotting dataframe] #######################
 
@@ -131,7 +133,7 @@ df2_long_join1 <- left_join(df2_inc_long, df2_z_long, suffix = c(".inc", ".z"), 
 df2_long <- left_join(df2_long_join1, df2_exc_long, by = "bot")
 
 # select for only key time steps
-df2_long_plot <- df2_long_join2 %>% 
+df2_long_plot <- df2_long %>% 
   select(time_step.inc, layer.inc, value.inc, value.z, value) %>% 
   rename(time_step = time_step.inc, layer = layer.inc, value.exc = value) %>% 
   filter(time_step == 0 | 
@@ -143,16 +145,16 @@ df2_long_plot <- df2_long_join2 %>%
 
 #'###################### [plotting] #######################
 
-#plot exc
+#plot inc
 ggplot(data = df2_long_plot, mapping = aes(y = value.z,
-                                      x = value.inc, 
-                                      group = time_step)) + 
+                                           x = value.inc, 
+                                           group = time_step)) + 
   geom_line(orientation = "y") +
   scale_y_reverse(name = "depth (m)") +
   scale_x_continuous(name = "Included component content (g/m2)") +
   facet_wrap(~time_step)
 
-#plot inc
+#plot exc
 ggplot(data = df2_long_plot, mapping = aes(y = value.z,
                                            x = value.exc, 
                                            group = time_step)) + 
@@ -166,9 +168,11 @@ ggplot(data = df2_long_plot, mapping = aes(y = value.z,
 
 
 # sum check to ensure conservation of mass (might be a problem w/ v.large diffusion rates)
-df2_cmass <- df2[["inc"]] %>%
+df2_cmass <- df2[["exc"]] %>%
   rowwise() %>%
   mutate(
     sum = sum(c_across(2:7))
   )
 df2_cmass[[9]]
+
+df2[["inc"]][601,7]
